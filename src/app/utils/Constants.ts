74e26b6b043f977cfe3809bca4f22c4fd9c1d823
MelvinJG <melvinjoj58@gmail.com>
2026-01-10 16:17:53 -0600

    Add phone restrictions and URL server

--- src/app/utils/Constants.ts
+++ src/app/utils/Constants.ts
@@ -1,4 +1,5 @@
 export const Constants = {
-    HOST_URL: 'https://57077ab053e9.ngrok-free.app',
+    //HOST_URL: 'https://57077ab053e9.ngrok-free.app'
     //HOST_URL: 'http://localhost:2200'
+    HOST_URL: 'https://fraternity-v2-server.onrender.com'
 };
